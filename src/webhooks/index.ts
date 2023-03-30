f8519398c8291ce2e2e7961dcc46dea845f84bfc
Duncan Snaith <duncan.snaith@redbrain.com>
2023-03-30 16:47:42 +0200

    fork shopify express

---
+++ src/webhooks/index.ts
@@ -0,0 +1,56 @@
+import express, {Request, Response} from 'express';
+import {AddHandlersParams, DeliveryMethod, Shopify} from '@shopify/shopify-api';
+
+import {AppConfigInterface} from '../config-types';
+import {ApiAndConfigParams} from '../types';
+import {AppInstallations} from '../app-installations';
+import {deleteAppInstallationHandler} from '../middlewares';
+
+import {
+  ProcessWebhooksMiddleware,
+  ProcessWebhooksMiddlewareParams,
+  WebhookHandlersParam,
+} from './types';
+import {process} from './process';
+
+export function processWebhooks({
+  api,
+  config,
+}: ApiAndConfigParams): ProcessWebhooksMiddleware {
+  return function ({webhookHandlers}: ProcessWebhooksMiddlewareParams) {
+
+    console.log("### REDBRAIN PATCH ### removing webhook mount");
+    // mountWebhooks(api, config, webhookHandlers);
+
+    return [
+      express.text({type: '*/*'}),
+      async (req: Request, res: Response) => {
+        await process({
+          req,
+          res,
+          api,
+          config,
+        });
+      },
+    ];
+  };
+}
+
+function mountWebhooks(
+  api: Shopify,
+  config: AppConfigInterface,
+  handlers: WebhookHandlersParam,
+) {
+  api.webhooks.addHandlers(handlers as AddHandlersParams);
+
+  // Add our custom app uninstalled webhook
+  const appInstallations = new AppInstallations(config);
+
+  api.webhooks.addHandlers({
+    APP_UNINSTALLED: {
+      deliveryMethod: DeliveryMethod.Http,
+      callbackUrl: config.webhooks.path,
+      callback: deleteAppInstallationHandler(appInstallations, config),
+    },
+  });
+}
